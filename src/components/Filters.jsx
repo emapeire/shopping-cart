1da930a9780252938f08927c1da0401864e4a292
Emanuel Peire <emapeire@outlook.com>
2023-11-17 20:12:37 -0300

    Add useId hook to Filters component

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react'
+import { useState, useId } from 'react'
 import './Filters.css'
 
 export default function Filters({ changeFilters }) {
   const [minPrice, setMinPrice] = useState(0)
+  const minPriceFilterId = useId()
+  const categoryFilterId = useId()
 
   const handleChangePrice = (event) => {
     setMinPrice(event.target.value)
@@ -25,10 +27,10 @@ export default function Filters({ changeFilters }) {
   return (
     <section className='filters'>
       <div>
-        <label htmlFor='price'>Min. price</label>
+        <label htmlFor={minPriceFilterId}>Min. price</label>
         <input
           type='range'
-          id='price'
+          id={minPriceFilterId}
           min='0'
           max='1000'
           onChange={handleChangePrice}
@@ -36,8 +38,8 @@ export default function Filters({ changeFilters }) {
         <output htmlFor='price'>${minPrice}</output>
       </div>
       <div>
-        <label htmlFor='category'>Category</label>
-        <select id='category' onChange={handleChangeCategory}>
+        <label htmlFor={categoryFilterId}>Category</label>
+        <select id={categoryFilterId} onChange={handleChangeCategory}>
           <option value='all'>All</option>
           <option value='laptops'>Laptops</option>
           <option value='smartphones'>Smartphones</option>
