5e537f89930b03245767aa6ec614eb21373bd5d8
Emanuel Peire <emapeire@outlook.com>
2023-11-18 22:04:50 -0300

    Refactor useCartReducer to use CART_ACTIONS_TYPES

--- src/hooks/useCartReducer.js
+++ src/hooks/useCartReducer.js
@@ -1,16 +1,20 @@
 import { useReducer } from 'react'
-import { cartReducer, cartInitialState } from '../reducers/CartReducer'
+import {
+  cartReducer,
+  cartInitialState,
+  CART_ACTIONS_TYPES
+} from '../reducers/CartReducer'
 
 export function useCartReducer() {
   const [state, dispatch] = useReducer(cartReducer, cartInitialState)
 
   const addToCart = (product) =>
-    dispatch({ type: 'ADD_TO_CART', payload: product })
+    dispatch({ type: CART_ACTIONS_TYPES.ADD_TO_CART, payload: product })
 
   const removeFromCart = (product) =>
-    dispatch({ type: 'REMOVE_FROM_CART', payload: product })
+    dispatch({ type: CART_ACTIONS_TYPES.REMOVE_FROM_CART, payload: product })
 
-  const clearCart = () => dispatch({ type: 'CLEAR_CART' })
+  const clearCart = () => dispatch({ type: CART_ACTIONS_TYPES.CLEAR_CART })
 
   return { state, addToCart, removeFromCart, clearCart }
 }
